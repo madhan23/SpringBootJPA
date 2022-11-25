20e3bba81a1b5e2fa1d1ddde7faaf9d520ca5714
madhan23 <madhanraju23@gmail.com>
2022-11-25 21:46:52 +0530

    update

--- src/main/java/com/dev/jpa/controller/PostController.java
+++ src/main/java/com/dev/jpa/controller/PostController.java
@@ -1,28 +1,57 @@
 package com.dev.jpa.controller;
 
-import com.dev.jpa.dto.PostDtoView;
-import com.dev.jpa.dto.PostRequestDto;
-import com.dev.jpa.dto.PostResponseDto;
-import com.dev.jpa.entity.Post;
-import com.dev.jpa.service.PostService;
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.jpa.dto.PostCommentRequest;
+import com.dev.jpa.dto.PostCommentResponse;
+import com.dev.jpa.dto.PostDtoView;
+import com.dev.jpa.dto.PostRequest;
+import com.dev.jpa.entity.Post;
+import com.dev.jpa.service.PostService;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
-@RequestMapping("api")
+@RequestMapping("api/posts")
 @AllArgsConstructor
+@Slf4j
 public class PostController {
 
     private final PostService postService;
-    @PostMapping("/posts")
-    public Post savePost(@RequestBody PostRequestDto request) {
+    @PostMapping("/")
+    public Post savePost(@RequestBody PostRequest request) {
+    	log.info(" Save Post payload {}",request);
         return postService.savePost(request);
     }
 
-    @GetMapping("/posts/{userId}")
-    public List<PostDtoView> getPost (@PathVariable final Integer userId){
-        return  postService.getPost(userId);
+    @GetMapping("/{userId}")
+    public List<PostDtoView> getAllPostByUser (@PathVariable final Integer userId){
+    	log.info("Fetch all Post Details   by User Id :: {}",userId);
+        return  postService.getAllPostByUser(userId);
     }
+    
+    
+    @GetMapping("/{postId}/comments")
+    public List<PostCommentResponse> getPostComments (@PathVariable final Integer postId){
+    	log.info("fetch all comments for particular post :: ",postId);
+        return  postService.getPostComments(postId);
+    }
+    
+    @PostMapping("/{postId}/comments")
+    public PostCommentResponse addCommentToPost (@PathVariable final Integer postId,@RequestBody PostCommentRequest request){	
+        return  postService.addCommentToPost(postId,request);
+    }
+    
+    
 }
+
+
+
